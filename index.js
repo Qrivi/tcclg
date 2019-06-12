e645e824cc4c116cf3d85181884ca41da0b66154
Kristof Dewilde <git@krivi.be>
2019-06-12 17:33:05 +0200

    Log to console _and_ to file

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const clear = require( 'clear' );
 const figlet = require( 'figlet' );
 const configUtils = require( './lib/configutils' );
 const requestUtils = require( './lib/requestutils' );
+const logUtils = require( './lib/logutils' );
 
 async function run(){
     if( await configUtils.verify( config ) ) {
-        console.log('\nConfiguration is OK. Let\'s get this bread.');
+        logUtils.log('\nConfiguration is OK. Let\'s get this bread.');
         config.projects.forEach( project => {
             if(project.include){
                 let issues = requestUtils.getChanges(project, config.teamcity);
@@ -19,13 +20,13 @@ async function run(){
             }
         });
     } else {
-        console.log( chalk.red('\nPlease fix your configuration and try again.') );
+        logUtils.error( chalk.red('\nPlease fix your configuration and try again.') );
         process.exit( 0 );
     }
 }
 
 clear();
-console.log( 
+logUtils.log( 
     chalk.blue( figlet.textSync( 'TCCLG', { font: 'speed'} )),
     chalk.blue('\nA gotta go fast changelog generator' )
 );
@@ -35,20 +36,20 @@ if( process.argv[ 2 ] ) {
     try {
         const configFile = fs.readFileSync( process.argv[ 2 ] );
         config = JSON.parse( configFile );
-        console.log( `Configuration file at ${process.argv[ 2 ]} will be used.` );
+        logUtils.log( `Configuration file at ${process.argv[ 2 ]} will be used.` );
     } catch ( err ) {
-        console.log( `Configuration file at ${process.argv[ 2 ]} could not be read.` );
+        logUtils.error( `Configuration file at ${process.argv[ 2 ]} could not be read.` );
     }
 }
 if( !config ) {
     config = require( './config/default.json' );
-    console.log( 'Loaded the default configuration.' );
+    logUtils.log( 'Loaded the default configuration.' );
 }
 
 try {
     run();
 } catch ( err ) {
-    console.log( chalk.red( 'An exception occured.' ) );
-    console.log( err );
+    logUtils.error( chalk.red( 'An exception occured.' ) );
+    logUtils.error( err );
     process.exit( 1 );
 }
