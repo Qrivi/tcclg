d2f4e4c79ed68eff12af7f91eceb41efacc62e25
Kristof Dewilde <git@krivi.be>
2019-06-13 23:23:54 +0200

    Added eslint

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 #!/usr/bin/env node
 
-const fs = require( 'fs' );
-const chalk = require( 'chalk' );
-const logUtils = require( './lib/logutils' );
-const app = require( './app' );
+const fs = require('fs')
+const chalk = require('chalk')
+const logUtils = require('./lib/logutils')
+const app = require('./app')
 
-logUtils.clearConsole();
-logUtils.console( chalk.blue( '_____________________________ _________' ) );
-logUtils.console( chalk.blue( '___  __/_  ____/_  ____/__  / __  ____/' ) );
-logUtils.console( chalk.blue( '__  /  _  /    _  /    __  /  _  / __  ' ) );
-logUtils.console( chalk.blue( '_  /   / /___  / /___  _  /___/ /_/ /  ' ) );
-logUtils.console( chalk.blue( '/_/    \\____/  \\____/  /_____/\\____/' ) );
-logUtils.console( chalk.blue( 'A gotta go fast changelog generator    ' ) );
-logUtils.log( 'Starting TCCLG.' );
-logUtils.newLine();
+logUtils.clearConsole()
+logUtils.console(chalk.blue('_____________________________ _________'))
+logUtils.console(chalk.blue('___  __/_  ____/_  ____/__  / __  ____/'))
+logUtils.console(chalk.blue('__  /  _  /    _  /    __  /  _  / __  '))
+logUtils.console(chalk.blue('_  /   / /___  / /___  _  /___/ /_/ /  '))
+logUtils.console(chalk.blue('/_/    \\____/  \\____/  /_____/\\____/'))
+logUtils.console(chalk.blue('A gotta go fast changelog generator    '))
+logUtils.log('Starting TCCLG.')
+logUtils.newLine()
 
-let config;
-if( process.argv[ 2 ] ) {
-    try {
-        config = JSON.parse( fs.readFileSync( process.argv[ 2 ] ) );
-        logUtils.log( `Configuration file at ${process.argv[ 2 ]} will be used.`, true );
-    } catch ( err ) {
-        logUtils.warn( `Configuration file at ${process.argv[ 2 ]} could not be read.`, true );
-    }
+let config
+if (process.argv[ 2 ]) {
+  try {
+    config = JSON.parse(fs.readFileSync(process.argv[ 2 ]))
+    logUtils.log(`Configuration file at ${process.argv[ 2 ]} will be used.`, true)
+  } catch (err) {
+    logUtils.warn(`Configuration file at ${process.argv[ 2 ]} could not be read.`, true)
+  }
 }
-if( !config ) {
-    config = require( './config/default.json' );
-    logUtils.log( 'Loaded the default configuration.', true );
+if (!config) {
+  config = require('./config/default.json')
+  logUtils.log('Loaded the default configuration.', true)
 }
 
-process.on( 'exit', () => { logUtils.newLine(); } );
-app.run( config ).catch( err => app.interrupt( err ) );
+process.on('exit', () => logUtils.newLine())
+app.run(config).catch(err => app.interrupt(err))
--- playground.js
+++ playground.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const file = fs.createWriteStream('./output/tcclg.log', {flags: 'a'});
+const fs = require('fs')
+const file = fs.createWriteStream('./output/tcclg.log', { flags: 'a' })
 
-for(let i=0; i<= 100; i++) {
-    console.log(i);
-  file.write(i +' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n');
+for (let i = 0; i <= 100; i++) {
+  console.log(i)
+  file.write(i + ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n')
 }
