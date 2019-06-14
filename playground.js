568c7fdea507943da38ca6eb647534c7e32e6084
Kristof Dewilde <git@krivi.be>
2019-06-14 17:20:30 +0200

    Fixed eslint config and decouraged console.log

--- playground.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs')
-const file = fs.createWriteStream('./output/tcclg.log', { flags: 'a' })
-
-for (let i = 0; i <= 100; i++) {
-  console.log(i)
-  file.write(i + ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n')
-}
