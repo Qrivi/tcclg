efae1ebcc68c03ef4f4333eb7dcff32ab7406b87
Kristof Dewilde <git@krivi.be>
2019-07-16 09:55:44 +0200

    Small things

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ async function run (config) {
     logUtils.log('Configuration is OK. Let\'s get this bread.')
 
     const changeData = []
-
     config.projects.forEach(project => {
       if (project.include) {
         let changes = requestUtils.getChanges(project, config.teamcity)
@@ -18,6 +17,7 @@ async function run (config) {
         }
       }
     })
+
     logUtils.newLine()
     printUtils.toJson(changeData, config.preferences)
     printUtils.toMarkdown(changeData, config.preferences)
@@ -29,18 +29,11 @@ async function run (config) {
 }
 
 async function interrupt (err) {
-  await sleep(2000)
   logUtils.clearConsole()
   logUtils.warn('(╯°□°）╯︵ ┻━┻', true)
   logUtils.error('There\'s an error in the code!!1!', true)
   logUtils.error(err.stack)
   logUtils.newLine()
-}
-
-function sleep (ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
 }
 
 module.exports = { run, interrupt }
